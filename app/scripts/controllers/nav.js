3a47814e56d0c59f862ae1c828332509fb0416f6
vasyl1 <vasyl1@umbc.edu>
2014-03-15 18:44:09 -0400

    errot with user

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -4,11 +4,10 @@ app.controller('NavCtrl', function ($scope, $location, Post) {
 	$scope.post = {url: 'http://', title: ''};
  
   $scope.submitPost = function () {
-	  Post.create($scope.post).then(function (ref) {
+	  Post.create($scope.post).then(function (postId) {
+		  $scope.post = {url: 'http://', title: ''};
 		  $location.path('/posts/' + ref.name());
-			$scope.post = {url: 'http://', title: ''};
 		});
 	};
-	
 	
 });
