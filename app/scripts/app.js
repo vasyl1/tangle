689aab2b1b4f32d9ec2ca29be8af63244bdefdbd
vasyl1 <vasyl1@umbc.edu>
2014-03-15 10:58:36 -0400

    section5; zero warnings, following line#

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,26 @@
 /* global app:true */
 'use strict';
+
 var app = angular.module('tangleApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'firebase'
 ]);
+
 app.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/posts.html',
         controller: 'PostsCtrl'
       })
+      .when ('/posts/:postId', {
+		    templateUrl: 'views/showpost.html',
+		    controller: 'PostViewCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+.constant ('FIREBASE_URL','https://glaring-fire-3204.firebaseio.com/');
